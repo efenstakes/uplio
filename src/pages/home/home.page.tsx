3789efefe3b6fe7a06b52c0762630e38c7f1a1a9
Efen Stakes <efenstakes101@gmail.com>
2022-03-22 18:22:58 +0300

    add contact section component

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,9 +1,16 @@
+
+
+// components
 import AppbarComponent from '../../components/appbar/appbar.component'
+import SectionTitleComponent from '../../components/section_title/section_title.component'
 import VSpacerComponent from '../../components/v_spacer/v_spacer.component'
 import CompaniesComponent from './companies.component'
-import './home.page.scss'
+import ContactComponent from './contact.component'
 import ProjectsComponent from './projects.component'
 import WelcomeComponent from './welcome.component'
+
+
+import './home.page.scss'
 
 
 
@@ -19,14 +26,25 @@ const HomePage = () => {
         <VSpacerComponent space={8} />
 
         {/* companies */}
+        <SectionTitleComponent title="COMPANIES" />
+        <VSpacerComponent space={2} />
         <CompaniesComponent />
         <VSpacerComponent space={8} />
 
         {/* projects */}
+        <SectionTitleComponent title="PROJECTS" />
+        <VSpacerComponent space={2} />
         <ProjectsComponent />
         <VSpacerComponent space={8} />
 
         {/* tools */}
+
+
+        {/* contact */}
+        <SectionTitleComponent title="CONTACT ME" />
+        <VSpacerComponent space={2} />
+        <ContactComponent />
+        <VSpacerComponent space={8} />
 
     </div>
   )
